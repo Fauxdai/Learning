2daba0e0ff3fadbc915cd63733bfc39920b07c04
Fox <fox@Foxs-MacBook-Pro.local>
2023-07-13 16:49:09 -0400

    More folders and some readme files.

---
+++ Projects/TinyProject/README.txt
@@ -0,0 +1,3 @@
+TinyProject aims to be a space where I do micro programming stuff. Maybe I make a portion of a installer script. Maybe it's a python script that converts a CSV to a PDF or Word Document. 
+
+In anycase, this is sort of a place to work on smaller pieces of larger puzzles. 
--- Python/Practice_Game/ChatGPTRoller.py
+++ Python/Practice_Game/ChatGPTRoller.py
@@ -1,37 +1,86 @@
 import random
+import os
 
-stats = ['Strength', 'Dexterity', 'Constitution', 'Wisdom', 'Intelligence', 'Charisma']
-stat_values = {}
+# Character Class Choices
+classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin',
+           'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard']
 
-def roll_stat():
-    return random.randint(1, 20)
+# Character Race Choices
+races = ['Dwarf', 'Elf', 'Halfling', 'Human', 'Dragonborn', 'Gnome', 'Half-Elf',
+         'Half-Orc', 'Tiefling']
 
-def display_stats():
-    print("\n----- Current Stats -----")
-    for stat in stats:
-        if stat in stat_values:
-            print(f"{stat}: {stat_values[stat]}")
-        else:
-            print(f"{stat}: Not rolled yet")
-    print("-------------------------\n")
+# Ability Scores Constants
+ABILITY_SCORES = ['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma']
+
+def roll_ability_score():
+    """Rolls a random ability score between 3 and 18"""
+    return random.randint(3, 18)
+
+def roll_ability_scores():
+    """Rolls ability scores for all six abilities"""
+    ability_scores = {}
+    for ability in ABILITY_SCORES:
+        ability_scores[ability] = roll_ability_score()
+    return ability_scores
+
+def select_class():
+    """Prompts the user to select a character class"""
+    clear_screen()
+    print("Select a Character Class:")
+    for index, char_class in enumerate(classes, start=1):
+        print(f"{index}. {char_class}")
+    while True:
+        choice = int(input("Enter the number corresponding to your choice: "))
+        if choice > 0 and choice <= len(classes):
+            return classes[choice - 1]
+        print("Invalid choice. Please try again.")
+
+def select_race():
+    """Prompts the user to select a character race"""
+    clear_screen()
+    print("Select a Character Race:")
+    for index, race in enumerate(races, start=1):
+        print(f"{index}. {race}")
+    while True:
+        choice = int(input("Enter the number corresponding to your choice: "))
+        if choice > 0 and choice <= len(races):
+            return races[choice - 1]
+        print("Invalid choice. Please try again.")
+
+def generate_character():
+    """Generates a new D&D character"""
+    clear_screen()
+    character = {}
+    character['Class'] = select_class()
+    character['Race'] = select_race()
+    character['Ability Scores'] = roll_ability_scores()
+    return character
+
+def display_character(character):
+    """Displays the character's details"""
+    clear_screen()
+    print("\n----- Character Details -----")
+    print(f"Class: {character['Class']}")
+    print(f"Race: {character['Race']}")
+    print("\nAbility Scores:")
+    for ability, score in character['Ability Scores'].items():
+        print(f"{ability}: {score}")
+    print("-----------------------------\n")
+
+def clear_screen():
+    """Clears the terminal/console screen"""
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
-    print("Welcome to the D&D Stat Roller!")
+    print("Welcome to the D&D Character Creator!")
     while True:
-        display_stats()
-        choice = input("Press R to roll a stat, E to exit: ").lower()
-
-        if choice == 'r':
-            stat = random.choice(stats)
-            stat_value = roll_stat()
-            stat_values[stat] = stat_value
-            print(f"\n{stat} rolled: {stat_value}\n")
-        elif choice == 'e':
-            print("\nExiting the D&D Stat Roller. Goodbye!")
+        character = generate_character()
+        display_character(character)
+        choice = input("Press 'R' to create another character or 'E' to exit: ").lower()
+        if choice == 'e':
+            clear_screen()
+            print("\nExiting the D&D Character Creator. Goodbye!")
             break
-        else:
-            print("\nInvalid choice. Please try again.\n")
 
 if __name__ == '__main__':
     main()
-
