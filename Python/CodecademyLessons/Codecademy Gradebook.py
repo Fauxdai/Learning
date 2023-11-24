a666f956c0b81f1a30362b0f8722246edb01e805
fox <fauxdai@gmail.com>
2023-11-24 00:00:28 -0500

    playing with python. New possible project

--- Python/CodecademyLessons/Codecademy Gradebook.py
+++ Python/CodecademyLessons/Codecademy Gradebook.py
@@ -7,8 +7,7 @@ grades = [98, 97, 85, 88]
 
 gradebook = [subjects[0], grades[0]], [subjects[1], grades[1]], [subjects[2], grades[2]],[subjects[3], grades[3]]
 
-print(gradebook)
-
-gradebook.append(["Computer Science"], [100])
+gradebook.append(["Computer Science", 100])
+gradebook.append(["visual arts", 93])
 
 print(gradebook)
