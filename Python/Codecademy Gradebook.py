9d98d322e3d97f1eca26d7cc23392f29aab8f3fc
fox <fauxdai@gmail.com>
2023-11-23 21:21:13 -0500

    Completed Codecademy Challenge - Gradebook

---
+++ Python/Codecademy Gradebook.py
@@ -0,0 +1,14 @@
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+subjects = ["physics", "calculus", "poetry", "history"]
+
+grades = [98, 97, 85, 88]
+
+gradebook = [subjects[0], grades[0]], [subjects[1], grades[1]], [subjects[2], grades[2]],[subjects[3], grades[3]]
+
+print(gradebook)
+
+gradebook.append(["Computer Science"], [100])
+
+print(gradebook)
